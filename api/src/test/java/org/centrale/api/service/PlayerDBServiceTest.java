8481caf7e3e836041753591036b5585ae0d53f6f
julien-sr <julien@searoutes.com>
2024-01-14 17:01:00 +0100

    api : works with H2 in memory database

---
+++ api/src/test/java/org/centrale/api/service/PlayerDBServiceTest.java
@@ -0,0 +1,24 @@
+package org.centrale.api.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.centrale.api.service.PlayerDBService;
+import org.centrale.domain.Player;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlayerDBServiceTest {
+
+    @Autowired
+    PlayerDBService dataService;
+
+    @Test
+    public void getPlayer1(){
+        Player p = dataService.getPlayerById(1);
+        assertEquals(1, p.getId());
+        assertEquals("Player1", p.getName());
+    }
+
+}
---
+++ domain/src/main/java/org/centrale/domain/Player.java
@@ -0,0 +1,13 @@
+package org.centrale.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Player {
+
+    Integer id;
+    String name;
+
+}
