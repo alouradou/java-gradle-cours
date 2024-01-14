d8edbc7a5c5d36d7647f2fb3e1f2051171149aab
julien-sr <julien@searoutes.com>
2024-01-14 18:45:49 +0100

    api : add entity

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,2 +1,3 @@
-CREATE TABLE players( id INT PRIMARY KEY, name VARCHAR(50));
-INSERT INTO players VALUES (1, 'Player1');
+SELECT 1;
+--CREATE TABLE players( id INT PRIMARY KEY, name VARCHAR(50));
+--INSERT INTO players VALUES (1, 'Player1');
