97906bae4ca712a83ac0aa85f3f7ef5eeaa8c410
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-01-15 09:49:22 +0100

    Spring Boot : Premières Routes

--- api/src/main/java/org/centrale/api/controller/ExampleController.java
+++ api/src/main/java/org/centrale/api/controller/ExampleController.java
@@ -1,15 +1,31 @@
 package org.centrale.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
 
+    @GetMapping("/")
+    public String hello(){
+        return "Hello World!";
+    }
+
     @GetMapping("test")
     public String play(@RequestParam String param){
         return param;
+    }
+
+    @PostMapping("test")
+    public String playPost(@RequestParam String param){
+        return param;
+    }
+
+    @GetMapping("test2")
+    public String play2(@RequestParam String user, @RequestParam String age){
+        return "Hello " + user + "! Tu as " + age + " an" + (Integer.parseInt(age) > 1 ? "s" : "") + " !";
     }
 
 }
