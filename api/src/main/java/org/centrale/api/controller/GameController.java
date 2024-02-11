66de08caf8019d57d56aff99462062a7523b1e68
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-02-11 23:29:43 +0100

    Testing GameService

--- api/src/main/java/org/centrale/api/controller/GameController.java
+++ api/src/main/java/org/centrale/api/controller/GameController.java
@@ -35,26 +35,11 @@ public class GameController {
     // POST /play?player1Id=ID1&player2Id=ID2&handPlayer1=HAND1&handPlayer2=HAND2 (le résultat devra etre persisté)
     @PostMapping("/play")
     public String gameTurn(@RequestParam Long player1Id, @RequestParam Long player2Id, @RequestParam String handPlayer1, @RequestParam String handPlayer2){
+        this.gameService.addPlayer(playerDBService.getPlayerEntity(player1Id).getName());
+        this.gameService.addPlayer(playerDBService.getPlayerEntity(player2Id).getName());
         this.gameService.play(player1Id, player2Id, handPlayer1, handPlayer2);
         return "Le score est : " + this.gameService.getScore(1) + " - " + this.gameService.getScore(2);
     }
 
-    @PostMapping("/player/remove")
-    public String gameRemovePlayer(@RequestParam String name) {
-        if(name.equals("1") || name.equals("2")){
-            this.gameService.removePlayer(Integer.parseInt(name));
-        }
-        else {
-            this.gameService.removePlayer(name);
-        }
-        return "Joueur supprimé !";
-    }
-
-//    @PostMapping("/play")
-//    public String gameTurn(@RequestParam String hand1, @RequestParam String hand2){
-//        this.gameService.playTurn(hand1, hand2);
-//        System.out.println(this.gameService.getScore(1));
-//        return "Le score est : " + this.gameService.getScore(1) + " - " + this.gameService.getScore(2);
-//    }
 
 }
